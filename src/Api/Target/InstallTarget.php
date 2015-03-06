6922cc890cb8a08d5207a9b618605fdf0a881b6a
Bernhard Schussek <bschussek@gmail.com>
2015-03-06 10:46:55 +0100

    Implemented InstallTarget

---
+++ src/Api/Target/InstallTarget.php
@@ -0,0 +1,197 @@
+<?php
+
+/*
+ * This file is part of the puli/web-resource-plugin package.
+ *
+ * (c) Bernhard Schussek <bschussek@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Puli\WebResourcePlugin\Api\Target;
+
+use OutOfBoundsException;
+use Puli\RepositoryManager\Assert\Assert;
+
+/**
+ * A target where resources can be installed.
+ *
+ * An install target has a name which identifies the target. Additionally, it
+ * has a location string which represents the location where the resources will
+ * be installed. The location can be a directory name, a URL or any other string
+ * that can be interpreted by a {@link ResourceInstaller}.
+ *
+ * Parameters can be set on the target to pass additional information to the
+ * installer that can not be obtained from the location string. Examples are
+ * user names or passwords and other, similar connection settings.
+ *
+ * An install target also has a URL format. This format defines the format of
+ * the URLs generated for resources installed in that target. For example, if
+ * targets are installed directly in the public directory, then you will set the
+ * URL format to "/%s". If resources are installed in the sub-directory
+ * "resources", the proper URL format is "/resources/%s". If resources are
+ * installed on another server or a CDN, you can set the URL format to the full
+ * domain: "http://my.cdn.com/%s".
+ *
+ * @since  1.0
+ * @author Bernhard Schussek <bschussek@gmail.com>
+ */
+class InstallTarget
+{
+    /**
+     * @var string
+     */
+    private $name;
+
+    /**
+     * @var string
+     */
+    private $installerName;
+
+    /**
+     * @var string
+     */
+    private $location;
+
+    /**
+     * @var string
+     */
+    private $urlFormat;
+
+    /**
+     * @var string[]
+     */
+    private $parameters;
+
+    /**
+     * Creates a new install target.
+     *
+     * @param string $name          The name of the target.
+     * @param string $installerName The name of the used installer.
+     * @param string $location      The location where resources are installed.
+     * @param string $urlFormat     The format of the generated resource URLs.
+     *                              Include the placeholder "%s" for the resource
+     *                              path relative to the target location.
+     * @param array  $parameters    Additional parameters to be processed by the
+     *                              resource installer or the URL generator.
+     */
+    public function __construct($name, $installerName, $location, $urlFormat = '/%s', array $parameters = array())
+    {
+        Assert::string($name, 'The target name must be a string. Got: %s');
+        Assert::notEmpty($name, 'The target name must not be empty.');
+        Assert::string($installerName, 'The installer name must be a string. Got: %s');
+        Assert::notEmpty($installerName, 'The installer name must not be empty.');
+        Assert::string($location, 'The target location must be a string. Got: %s');
+        Assert::notEmpty($location, 'The target location must not be empty.');
+        Assert::string($urlFormat, 'The target URL format must be a string. Got: %s');
+        Assert::notEmpty($urlFormat, 'The target URL format must not be empty.');
+
+        $this->name = $name;
+        $this->installerName = $installerName;
+        $this->location = $location;
+        $this->urlFormat = $urlFormat;
+        $this->parameters = $parameters;
+    }
+
+    /**
+     * Returns the target name.
+     *
+     * @return string The target name.
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Returns the name of the used installer.
+     *
+     * @return string The installer name.
+     */
+    public function getInstallerName()
+    {
+        return $this->installerName;
+    }
+
+    /**
+     * Returns the target location.
+     *
+     * The target location can be a directory name, a URL or any other string
+     * that can be understood by a {@link ResourceInstaller}.
+     *
+     * @return string The target location.
+     */
+    public function getLocation()
+    {
+        return $this->location;
+    }
+
+    /**
+     * Returns the format of the generated resource URLs.
+     *
+     * The format contains the placeholder "%s" where the resource path relative
+     * to the target location is inserted.
+     *
+     * @return string The URL format.
+     */
+    public function getUrlFormat()
+    {
+        return $this->urlFormat;
+    }
+
+    /**
+     * Returns the value of the given parameter.
+     *
+     * @param string $name The parameter name.
+     *
+     * @return mixed The parameter value.
+     *
+     * @throws OutOfBoundsException If the parameter does not exist.
+     */
+    public function getParameter($name)
+    {
+        if (!isset($this->parameters[$name])) {
+            throw new OutOfBoundsException(sprintf(
+                'The target parameter "%s" does not exist. Did you forget to set it?',
+                $name
+            ));
+        }
+
+        return $this->parameters[$name];
+    }
+
+    /**
+     * Returns the values of all parameters.
+     *
+     * @return string[] The parameter values indexed by the parameter names.
+     */
+    public function getParameters()
+    {
+        return $this->parameters;
+    }
+
+    /**
+     * Returns whether the target has a given parameter.
+     *
+     * @param string $name The parameter name.
+     *
+     * @return bool Returns `true` if the given parameter exists and `false`
+     *              otherwise.
+     */
+    public function hasParameter($name)
+    {
+        return isset($this->parameters[$name]);
+    }
+
+    /**
+     * Returns whether the target has any parameters.
+     *
+     * @return bool Returns `true` if any parameters are set for the target and
+     *              `false` otherwise.
+     */
+    public function hasParameters()
+    {
+        return count($this->parameters) > 0;
+    }
+}
---
+++ tests/Api/Target/InstallTargetTest.php
@@ -0,0 +1,178 @@
+<?php
+
+/*
+ * This file is part of the puli/web-resource-plugin package.
+ *
+ * (c) Bernhard Schussek <bschussek@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Puli\WebResourcePlugin\Tests\Api\Target;
+
+use PHPUnit_Framework_TestCase;
+use Puli\WebResourcePlugin\Api\Target\InstallTarget;
+
+/**
+ * @since  1.0
+ * @author Bernhard Schussek <bschussek@gmail.com>
+ */
+class InstallTargetTest extends PHPUnit_Framework_TestCase
+{
+    public function testCreate()
+    {
+        $target = new InstallTarget('local', 'symlink', 'web/assets', '/assets/%s', array('user' => 'webmozart'));
+
+        $this->assertSame('local', $target->getName());
+        $this->assertSame('symlink', $target->getInstallerName());
+        $this->assertSame('web/assets', $target->getLocation());
+        $this->assertSame('/assets/%s', $target->getUrlFormat());
+        $this->assertSame(array('user' => 'webmozart'), $target->getParameters());
+    }
+
+    public function testCreateWithDefaultUrlFormat()
+    {
+        $target = new InstallTarget('local', 'symlink', 'web/assets');
+
+        $this->assertSame('/%s', $target->getUrlFormat());
+        $this->assertSame(array(), $target->getParameters());
+    }
+
+    /**
+     * @expectedException \InvalidArgumentException
+     */
+    public function testFailIfNameNull()
+    {
+        new InstallTarget(null, 'symlink', 'web/assets');
+    }
+
+    /**
+     * @expectedException \InvalidArgumentException
+     */
+    public function testFailIfNameEmpty()
+    {
+        new InstallTarget('', 'symlink', 'web/assets');
+    }
+
+    /**
+     * @expectedException \InvalidArgumentException
+     */
+    public function testFailIfNameNoString()
+    {
+        new InstallTarget(1234, 'symlink', 'web/assets');
+    }
+
+    /**
+     * @expectedException \InvalidArgumentException
+     */
+    public function testFailIfInstallerNameNull()
+    {
+        new InstallTarget('local', null, 'web/assets');
+    }
+
+    /**
+     * @expectedException \InvalidArgumentException
+     */
+    public function testFailIfInstallerNameEmpty()
+    {
+        new InstallTarget('local', '', 'web/assets');
+    }
+
+    /**
+     * @expectedException \InvalidArgumentException
+     */
+    public function testFailIfInstallerNameNoString()
+    {
+        new InstallTarget('local', 1234, 'web/assets');
+    }
+
+    /**
+     * @expectedException \InvalidArgumentException
+     */
+    public function testFailIfLocationNull()
+    {
+        new InstallTarget('local', 'symlink', null);
+    }
+
+    /**
+     * @expectedException \InvalidArgumentException
+     */
+    public function testFailIfLocationEmpty()
+    {
+        new InstallTarget('local', 'symlink', '');
+    }
+
+    /**
+     * @expectedException \InvalidArgumentException
+     */
+    public function testFailIfLocationNoString()
+    {
+        new InstallTarget('local', 'symlink', 1234);
+    }
+
+    /**
+     * @expectedException \InvalidArgumentException
+     */
+    public function testFailIfUrlFormatNull()
+    {
+        new InstallTarget('local', 'symlink', 'web/assets', null);
+    }
+
+    /**
+     * @expectedException \InvalidArgumentException
+     */
+    public function testFailIfUrlFormatEmpty()
+    {
+        new InstallTarget('local', 'symlink', 'web/assets', '');
+    }
+
+    /**
+     * @expectedException \InvalidArgumentException
+     */
+    public function testFailIfUrlFormatNoString()
+    {
+        new InstallTarget('local', 'symlink', 'web/assets', 1234);
+    }
+
+    public function testGetParameter()
+    {
+        $target = new InstallTarget('local', 'symlink', 'web', '/%s', array('param1' => 'value1', 'param2' => 'value2'));
+
+        $this->assertSame('value1', $target->getParameter('param1'));
+        $this->assertSame('value2', $target->getParameter('param2'));
+    }
+
+    /**
+     * @expectedException \OutOfBoundsException
+     */
+    public function testGetParameterFailsIfNotFound()
+    {
+        $target = new InstallTarget('local', 'symlink', 'web');
+
+        $target->getParameter('foobar');
+    }
+
+    public function testHasParameter()
+    {
+        $target = new InstallTarget('local', 'symlink', 'web', '/%s', array('param1' => 'value1', 'param2' => 'value2'));
+
+        $this->assertTrue($target->hasParameter('param1'));
+        $this->assertTrue($target->hasParameter('param2'));
+        $this->assertFalse($target->hasParameter('foo'));
+    }
+
+    public function testHasParameters()
+    {
+        $target = new InstallTarget('local', 'symlink', 'web', '/%s', array('param1' => 'value1'));
+
+        $this->assertTrue($target->hasParameters());
+    }
+
+    public function testHasNoParameters()
+    {
+        $target = new InstallTarget('local', 'symlink', 'web', '/%s', array());
+
+        $this->assertFalse($target->hasParameters());
+    }
+}
